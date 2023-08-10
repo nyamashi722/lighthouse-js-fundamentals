f553d1cdecdb8fe9c2770167d8ccd81f8fe40ddc
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-08-10 16:30:14 -0400

    function exercise

---
+++ hello.js
@@ -0,0 +1,9 @@
+let sayHello = function (name) {
+  console.log("Hello world" + name)
+}
+sayHello("bob");
+
+function greeting(person) {
+  console.log("greetings!" + person)
+}
+greeting("rachey");
